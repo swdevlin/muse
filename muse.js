96b6789c32b2d4173425c96524cc21b34f052623
swdevlin <swdevlin@gmail.com>
2021-10-13 23:55:39 -0400

    DRY

--- muse.js
+++ muse.js
@@ -3,65 +3,37 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 const YAML = require('yaml');
 
-const version = '0.2';
+const version = '0.3';
+
+const keyJoin = (obj) => {
+    return Object.keys(obj).join(', ');
+}
 
 const muse_prefix = process.env.MUSE_PREFIX || 'muse';
 
+const files = fs.readdirSync(path.resolve(__dirname, './data/'));
+
+const yamlFiles = files.filter(function (file) {
+    return path.extname(file) === ".yaml" && path.basename(file) !== 'muse.yaml';
+});
+
 let file = fs.readFileSync( path.resolve(__dirname, './data/muse.yaml'), 'utf8');
-const m = YAML.parse(file);
+let muse = YAML.parse(file);
 
-file = fs.readFileSync(path.resolve(__dirname, './data/skills.yaml'), 'utf8');
-const skills = YAML.parse(file);
-
-file = fs.readFileSync(path.resolve(__dirname, './data/morphs.yaml'), 'utf8');
-const morphs = YAML.parse(file);
-
-file = fs.readFileSync(path.resolve(__dirname, './data/bio-morphs.yaml'), 'utf8');
-const bioMorphs = YAML.parse(file);
-
-file = fs.readFileSync(path.resolve(__dirname, './data/info-morphs.yaml'), 'utf8');
-const infoMorphs = YAML.parse(file);
-
-file = fs.readFileSync(path.resolve(__dirname, './data/uplift-morphs.yaml'), 'utf8');
-const upliftMorphs = YAML.parse(file);
-
-file = fs.readFileSync(path.resolve(__dirname, './data/synth-morphs.yaml'), 'utf8');
-const synthMorphs = YAML.parse(file);
-
-file = fs.readFileSync(path.resolve(__dirname, './data/pod-morphs.yaml'), 'utf8');
-const podMorphs = YAML.parse(file);
-
-file = fs.readFileSync(path.resolve(__dirname, './data/aptitudes.yaml'), 'utf8');
-const aptitudes = YAML.parse(file);
-
-file = fs.readFileSync(path.resolve(__dirname, './data/ware.yaml'), 'utf8');
-const wares = YAML.parse(file);
-
-const campaignFile = path.resolve(__dirname, './campaign.yaml');
-let campaign = {};
-if (fs.existsSync(campaignFile)) {
-    file = fs.readFileSync(campaignFile, 'utf8');
-    campaign = YAML.parse(file);
+for (const name of yamlFiles) {
+    let file = fs.readFileSync( path.resolve(__dirname, 'data', name), 'utf8');
+    const y = YAML.parse(file);
+    if (y.hasOwnProperty('entries')) {
+        muse = Object.assign(muse, y.entries);
+        let t = y.text.trim();
+        muse[y.topic] = {
+            title: y.title,
+            text: `${t} ${keyJoin(y.entries)}`
+        }
+    } else
+        muse = Object.assign(muse, y);
 }
 
-const muse = Object.assign(
-  {}, m,
-  skills, aptitudes,
-  morphs, bioMorphs, infoMorphs, upliftMorphs, synthMorphs, podMorphs,
-  campaign, wares
-);
-muse['skills'] = {title: 'Skills', text: 'The skills I know about are: ' + Object.keys(skills).join(', ')};
-muse['aptitudes'] = {title: 'Aptitudes', text: 'Your aptitudes represent your natural, inherent abilities. There are 6 aptitude scores: ' + Object.keys(aptitudes).join(', ')};
-muse['ware'] = {
-    title: 'Aptitudes',
-    text: `Ware is a catch-all category for augmentations of different kinds. Unless otherwise noted, each ware item can only be installed in the same morph once, no matter if it is available in different forms.`
-};
-muse['biomorphs'].text += '\nCommon morphs are: ' + Object.keys(bioMorphs).join(', ');
-muse['infomorphs'].text += '\nCommon morphs are: ' + Object.keys(infoMorphs).join(', ');
-muse['podmorphs'].text += '\nCommon morphs are: ' + Object.keys(podMorphs).join(', ');
-muse['synthmorphs'].text += '\nCommon morphs are: ' + Object.keys(synthMorphs).join(', ');
-muse['upliftmorphs'].text += '\nCommon morphs are: ' + Object.keys(upliftMorphs).join(', ');
-muse['ware'].text += '\nAvailable augmentation wares are: ' + Object.keys(wares).join(', ');
 muse['muse'].text += ` I am version ${version}.`;
 
 module.exports = muse;
