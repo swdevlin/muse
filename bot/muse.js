3d7b1c7a664d62a5bb25cda4f428022dd9f46777
swdevlin <swdevlin@gmail.com>
2021-11-06 12:44:29 -0400

    version increase

--- bot/muse.js
+++ bot/muse.js
@@ -5,7 +5,7 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 const YAML = require('yaml');
 
-const version = '0.5';
+const version = '0.6';
 
 const files = fs.readdirSync(path.resolve(__dirname, './data/'));
 
