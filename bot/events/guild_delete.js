0eab2af4fd176c7ffcba4c9d7c1519ac11c91e40
swdevlin <swdevlin@gmail.com>
2022-09-01 22:59:12 -0400

    Fix guild delete

--- bot/events/guild_delete.js
+++ bot/events/guild_delete.js
@@ -7,10 +7,10 @@ const guildDelete = async (guild) => {
   try {
     let channels = await knex('channel').select('id').where({guild_id: guild.id});
     for (const channel of channels) {
-      await trx('channel_topic').delete().where({channel_id: channel.id});
-      await trx('channel').delete().where({id: channel.id});
+      await knex('channel_topic').delete().where({channel_id: channel.id});
+      await knex('channel').delete().where({id: channel.id});
     }
-    await trx('guild').delete().where({id: guild.id});
+    await knex('guild').delete().where({id: guild.id});
     logger.info(`Removed from server ${guild.id}`);
   } catch(err) {
     logger.error(err);
